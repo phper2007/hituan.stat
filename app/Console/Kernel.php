d56883660a6b00202413d861e738eeae6f2a6e0d
liubo <liubo9@xin.com>
2018-12-12 15:03:50 +0800

    增加作业

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('updateExpress')->cron('1 7,12,16 * * * *');
+        $schedule->command('updateExpress')->cron('1 7,12,16,19 * * * *');
     }
 
     /**
